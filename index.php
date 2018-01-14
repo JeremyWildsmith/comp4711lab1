138ad81cd2be3ef57ffd79972f66f122f88ff59d
Jeremy Wildsmith <jeremywildsmith@yahoo.ca>
2018-01-14 15:39:58 -0800

    Sorted students.

--- index.php
+++ index.php
@@ -30,6 +30,9 @@
         $second->add_grade(50);
         $students['a456'] = $second;
         
+        
+        ksort($students);
+        
         foreach ($students as $student)
             echo $student->toString();
         ?>
